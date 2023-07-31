2427e6120c41daea111dc093e3bfcb8a8725b222
FDK <fudongkaikaifa@163.com>
2023-07-31 22:42:26 +0800

    渐变色的边框

--- mycoolproject/lib/app/modules/home/views/home_view.dart
+++ mycoolproject/lib/app/modules/home/views/home_view.dart
@@ -104,6 +104,23 @@ class _HomeListViewState extends State<HomeListView> {
                         style: TextStyle(fontSize: 25, color: Colors.black)),
                   ),
                   Positioned(
+                      left: 10,
+                      right: 10,
+                      bottom: 2,
+                      child: Container(
+                        height: 90,
+                        child: LayoutBuilder(
+                          builder: (context, bc) {
+                            return CustomPaint(
+                              painter: GradientBoundPainter(
+                                  colors: [Colors.red, Colors.blue],
+                                  width: bc.maxWidth,
+                                  height: bc.maxHeight),
+                            );
+                          },
+                        ),
+                      )),
+                  Positioned(
                       right: 10,
                       left: 10,
                       bottom: 0,
@@ -114,5 +131,50 @@ class _HomeListViewState extends State<HomeListView> {
                 ],
               ),
             )));
+  }
+}
+
+/// 渐变色的边框
+class GradientBoundPainter extends CustomPainter {
+  final List<Color> colors;
+  final double width;
+  final double height;
+  final double strokeWidth;
+  const GradientBoundPainter({
+    Key? key,
+    required this.colors,
+    required this.width,
+    required this.height,
+    this.strokeWidth = 2.0,
+  });
+  @override
+  void paint(Canvas canvas, Size size) {
+    //定义矩形的宽高
+    final rectWidth = width, rectHeight = height;
+    //圆角。必要的话可以将其作为变量传进来
+    final radius = 10.0;
+    //定义矩形的位置和尺寸
+    Rect rect = Offset(
+            size.width / 2 - rectWidth / 2, size.height / 2 - rectHeight / 2) &
+        Size(rectWidth, rectHeight);
+    //RRect.fromRectAndRadius一个具有圆角的矩形
+    RRect rRect = RRect.fromRectAndRadius(rect, Radius.circular(radius));
+    //绘制
+    final paint = Paint()
+      //创建线性渐变着色器
+      ..shader = LinearGradient(
+        begin: Alignment.centerLeft,
+        end: Alignment.centerRight,
+        colors: colors,
+      ).createShader(rect)
+      ..strokeWidth = strokeWidth
+      //只绘制边框而不填充
+      ..style = PaintingStyle.stroke;
+    canvas.drawRRect(rRect, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant GradientBoundPainter oldDelegate) {
+    return oldDelegate.colors != colors;
   }
 }
